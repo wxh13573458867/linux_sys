5ce34ab85682fc16bfc949f04f7b8af2299c5a2d
wxh <13573458867@163.com>
2021-07-15 23:22:06 +0800

    循环队列头文件加入注释

--- linux_loop_queue/loop_queue.h
+++ linux_loop_queue/loop_queue.h
@@ -24,13 +24,36 @@ typedef struct _LoopQueue
 	pthread_cond_t condConsumer;	
 } *LoopQueue;
 
-
+/*
+ *@brief	初始化循环队列
+ *@param:capacity	循环队列容量
+ *@param:nodeSize	单个节点大小
+ *@param:isClog	队列读取是否阻塞
+ *@reruen	成功:循环队列指针	失败:NULL
+ **/
 extern LoopQueue LoopQueue_Init(const int capacity, const int nodeSize, const int isClog);
 
+/*
+ *@brief	销毁循环队列
+ *@param:lQ	循环队列指针
+ * return	无
+ **/
 extern void LoopQueue_Destroy(LoopQueue lQ);
 
+/*
+ *@brief	向循环队列插入节点
+ *@param:lQ	循环队列指针
+ *@param:data	节点数据
+ *return	成功:0	失败:-1
+ **/
 extern int LoopQueue_Push(LoopQueue lQ, const void *data);
 
+/*
+ *@brief	从循环队列取出节点
+ *@param:lQ	循环队列指针
+ *@param:data	节点数据
+ *return	成功:0	失败:-1
+ **/
 extern int LoopQueue_Pop(LoopQueue lQ, void *data);
 
 #endif
